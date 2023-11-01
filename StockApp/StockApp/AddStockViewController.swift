f80c999f3d5f7e2d536ebea168809d5e4e34cecc
xintao zhang <zxt19990615@126.com>
2023-11-01 14:32:03 -0700

    enter stock symbol

---
+++ StockApp/StockApp/AddStockViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AddStockViewController.swift
+//  StockApp
+//
+//  Created by xintao zhang on 11/1/23.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+class AddStockViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
