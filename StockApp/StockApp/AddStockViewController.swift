49ceb34a594fd5f98207722fab165f21646c76eb
xintao zhang <zxt19990615@126.com>
2023-11-01 14:42:23 -0700

    3

--- StockApp/StockApp/AddStockViewController.swift
+++ StockApp/StockApp/AddStockViewController.swift
@@ -11,11 +11,76 @@ import SwiftyJSON
 
 class AddStockViewController: UIViewController {
 
+    @IBOutlet weak var txtStock: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func addStockAction(_ sender: Any) {
+        guard let stockSymbol = txtStock.text else {return}
 
+                                
+
+                                let url = "\(baseURL)profile/\(stockSymbol)?apikey=\(apiKey)"
+
+                                
+
+                AF.request(url).responseJSON { response in
+                    
+                    
+                    
+                    if(response.error != nil){
+                        
+                        print(response.error?.localizedDescription)
+                        
+                        return
+                        
+                    }
+                    
+                    // We have valid data here
+                    
+                    guard let rawData = response.data else {return}
+                    
+                    guard let jsonArray = JSON(rawData).array else {return}
+                    
+                    guard let stock = jsonArray.first else {return}
+                    
+                    
+                    
+                    let symbol = stock["symbol"].stringValue
+                    
+                    let price = stock["price"].floatValue
+                    
+                    let companyName = stock["companyName"].stringValue
+                    
+                    let description = stock["description"].stringValue
+                    
+                    
+                    
+                    print(symbol)
+                    
+                    print(price)
+                    
+                    print(companyName)
+                    
+                    print(description)
+                    
+                    //            let stockClass = StockClass()
+                    
+                    //            stockClass.symbol = symbol
+                    
+                    //            stockClass.price = price
+                    
+                    //            stockClass.companyName = companyName
+                    
+                    //            stockClass.desc = description
+                    
+                    //            self.addStockToDB(stockClass)
+                    
+                    
+                    
+                }    }
+    
 }
---
+++ StockApp/StockApp/Utils/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  StockApp
+//
+//  Created by xintao zhang on 11/1/23.
+//
+
+import Foundation
+
+let apiKey = "VCyJclBfdLMhmC7HXOYObXcatxwXNJ1o"
+let baseURL = "https://financialmodelingprep.com/api/v3"
