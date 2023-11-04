638c40c0945d5a087a9bddfd25910a131db228a0
xintao zhang <zxt19990615@126.com>
2023-11-04 14:36:41 -0700

    11.4 1

--- StockApp/StockApp/AddStockViewController.swift
+++ StockApp/StockApp/AddStockViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 import Alamofire
 import SwiftyJSON
+import RealmSwift
 
 class AddStockViewController: UIViewController {
 
---
+++ StockApp/StockApp/StockClass.swift
@@ -0,0 +1,8 @@
+//
+//  StockClass.swift
+//  StockApp
+//
+//  Created by xintao zhang on 11/4/23.
+//
+
+import Foundation
