7f582fc483ed928f11f6daf6b8fbbca782479e93
xintao zhang <zxt19990615@126.com>
2023-11-04 14:44:54 -0700

    11

--- StockApp/StockApp/StockClass.swift
+++ StockApp/StockApp/StockClass.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+import RealmSwift
+
+class StockClass: Object{
+    @objc dynamic var symbol: String = ""
+    @objc dynamic var price: Float = 0.9
+    @objc dynamic var companyName: String = ""
+    @objc dynamic var desc: String = ""
+    
+    override static func primaryKey() -> String? {
+        return "symbol"
+    }
+}
+
