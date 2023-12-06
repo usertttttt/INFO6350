9a84347f882aa0373edcbb781d7bb9618b6bde65
xintao zhang <zxt19990615@126.com>
2023-12-05 20:28:23 -0800

    api 1

--- StockAppFinal/StockAppFinal/ViewController.swift
+++ StockAppFinal/StockAppFinal/ViewController.swift
@@ -7,13 +7,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UITableViewDelegate, UITableViewDataSource{
 
+    var stockValues: [String] = [String]()
+
+    @IBOutlet weak var tblView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func getStockValues(_ sender: Any) {
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return stockValues.count
+        }
+        
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
 
+        cell.textLabel?.text = stockValues[indexPath.row]
+            return cell
+        }
 }
 
