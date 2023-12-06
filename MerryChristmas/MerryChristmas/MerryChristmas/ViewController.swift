3b37076bb54b863074db7eecb2befe47ef9f0189
xintao zhang <zxt19990615@126.com>
2023-12-05 18:21:54 -0800

    1

--- MerryChristmas/MerryChristmas/MerryChristmas/ViewController.swift
+++ MerryChristmas/MerryChristmas/MerryChristmas/ViewController.swift
@@ -7,13 +7,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UITableViewDelegate, UITableViewDataSource {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    let cities = ["New York", "London", "Paris", "Seattle"]
+
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            // Do any additional setup after loading the view.
+        }
+
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return cities.count
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell =   Bundle.main.loadNibNamed("ChristmasTableViewCell", owner: self)?.first as! ChristmasTableViewCell
+            cell.imageView?.image = UIImage(named: "Christmas\(indexPath.row)")
+            cell.lblChristmas.text = cities[indexPath.row]
+            return cell
+        }
+
     }
-
-
-}
-
